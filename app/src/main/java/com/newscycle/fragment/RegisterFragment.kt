ec64444c32c949f72fb39f49d627d371bce0bfdd
Dushane Smith <dushanes@buffalo.edu>
2020-02-13 17:04:04 -0500

    Added myfeed, popular, and categories class fragment skeleton and UI skeletons. Set home to portrait only.

--- app/src/main/java/com/newscycle/fragment/RegisterFragment.kt
+++ app/src/main/java/com/newscycle/fragment/RegisterFragment.kt
@@ -1,19 +1,14 @@
-package com.newscycle
+package com.newscycle.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.google.firebase.auth.FirebaseAuth
+import com.newscycle.R
 
 
 class RegisterFragment : Fragment(){
-    private val mAuth: FirebaseAuth= FirebaseAuth.getInstance()
-    private val TAG: String = "Register-Fragment:"
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -21,5 +16,4 @@ class RegisterFragment : Fragment(){
     ): View? {
         return inflater.inflate(R.layout.fragment_new_user, container, false)
     }
-
 }
